a99873601320722535553384c95d9eb4c5b41767
JoelGutierrezN <al221910321@gmail.com>
2021-01-17 01:16:29 -0600

    Factory y Seeders de Productos

--- database/seeders/productos.php
+++ database/seeders/productos.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class productos extends Seeder
 {
@@ -13,6 +16,15 @@ class productos extends Seeder
      */
     public function run()
     {
-        //
+        for($i = 0; $i<=100; $i++){
+            DB::table('producto')->insert([
+                'clave' => Str::random(25),
+                'producto' => Str::random(15),
+                'existencias' => $i, 
+                'precio_unitario' => $i + 50.00,
+                'unidad_medida' => 'Pieza',
+                'estatus' => 'Activo'
+            ]);
+        }
     }
 }
