8d958276db92c2a39f578baf14cf13c9ff0c995f
JoelGutierrezN <al221910321@gmail.com>
2021-01-17 02:23:20 -0600

    Listado de Productos

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $productos = DB::table('Producto')->get();
+    return view('welcome',[
+        'productos' => $productos
+    ]);
 });
 
 Auth::routes();
